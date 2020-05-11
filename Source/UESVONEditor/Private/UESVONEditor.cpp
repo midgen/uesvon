c34e6508ff558c194db7e5d22abe72d4cffe5f99
Chris Ashworth <chris@cashworth.net>
2020-05-11 20:46:44 +0100

    Fix includes for buildsettings V2

--- Source/UESVONEditor/Private/UESVONEditor.cpp
+++ Source/UESVONEditor/Private/UESVONEditor.cpp
@@ -1,6 +1,7 @@
-#include "UESVONEditor.h"
-#include "SVONVolumeDetails.h"
-#include "Editor/PropertyEditor/Public/PropertyEditorModule.h"
+#include "UESVONEditor/UESVONEditor.h"
+#include "UESVONEditor/Private/SVONVolumeDetails.h"
+
+#include <Editor/PropertyEditor/Public/PropertyEditorModule.h>
 
 IMPLEMENT_GAME_MODULE(FUESVONEditorModule, UESVONEditor);
 
@@ -14,23 +15,13 @@ void FUESVONEditorModule::StartupModule()
 
 	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
 
-	//PropertyModule.RegisterCustomPropertyTypeLayout(ASVONVolume::StaticClass(), FOnGetDetailCustomizationInstance::CreateRaw(&FSVONVolumeDetails::MakeInstance));
-
 	PropertyModule.RegisterCustomClassLayout("SVONVolume", FOnGetDetailCustomizationInstance::CreateStatic(&FSVONVolumeDetails::MakeInstance));
-
-
-	//create your factory and shared pointer to it.
-	//TSharedPtr<FGOAPAtomPinFactory> GOAPAtomPinFactory = MakeShareable(new FGOAPAtomPinFactory());
-	//and now register it.
-	//FEdGraphUtilities::RegisterVisualPinFactory(GOAPAtomPinFactory);
 }
 
 void FUESVONEditorModule::ShutdownModule()
 {
 	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
 
-	//Custom properties
-	//PropertyModule.UnregisterCustomPropertyTypeLayout("GOAPAtom");
 	UE_LOG(UESVONEditor, Warning, TEXT("UESVONEditorModule: Log Ended"));
 }
 
